6924da5e6c441cb78d7a4195670657a1589c8f53
Ethan Tuning <etuning5@hotmail.com>
2019-04-12 13:21:52 -0700

    stubbed out all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,19 @@ import { AppComponent } from './app.component';
 import { MainSidenavComponent } from './main-sidenav/main-sidenav.component';
 import { SubSidenavComponent } from './sub-sidenav/sub-sidenav.component';
 import { LoginComponent } from './login/login.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { ActionComponent } from './action/action.component';
+import { TopnavComponent } from './topnav/topnav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainSidenavComponent,
     SubSidenavComponent,
-    LoginComponent
+    LoginComponent,
+    TopbarComponent,
+    ActionComponent,
+    TopnavComponent
   ],
   imports: [
     BrowserModule,
