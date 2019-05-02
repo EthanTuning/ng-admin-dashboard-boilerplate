313299dfec75295c02dcbacc571ece1b9ffaee08
Ethan Tuning <etuning5@hotmail.com>
2019-05-02 15:20:28 -0700

    not sure

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { TopbarComponent } from '../_components/topbar/topbar.component';
+import { TopnavComponent } from '../_components/topnav/topnav.component';
+import { MainSidenavComponent } from '../_components/main-sidenav/main-sidenav.component';
+import { SubSidenavComponent } from '../_components/sub-sidenav/sub-sidenav.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
 
   constructor() { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
 import { AuthenticationService } from '../_services/authentication.service';
 
 @Component({
